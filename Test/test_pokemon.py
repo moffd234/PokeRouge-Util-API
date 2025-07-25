f57e4298682b73d9ffd7c1d390505dbf3bc1ab06
Daniel Moffett <moffd234@gmail.com>
2025-07-25 18:36:02 -0400

    Wrote test_pokemon_to_dict

--- Test/test_pokemon.py
+++ Test/test_pokemon.py
@@ -26,3 +26,47 @@ def test_pokemon_constructor(example_pokemon):
     assert example_pokemon.growth_rate == "MEDIUM_SLOW"
     assert example_pokemon.male_percent == 87.5
     assert example_pokemon.gender_diffs == False
+
+def test_pokemon_to_dict(example_pokemon):
+    expected: dict = {
+            id: 1,
+            "name": "BULBASAUR",
+            "generation": 1,
+            "category": "Seed Pokemon",
+            "legendary_status": {
+                "sub_legendary": False,
+                "legendary": False,
+                "mythical": False,
+            },
+            "types": {
+                "type_1": "GRASS",
+                "type_2": "POISON",
+            },
+            "abilities": {
+                "ability_1": "OVERGROW",
+                "ability_2": None,
+                "hidden_ability": "CHLOROPHYLL",
+            },
+            "size": {
+                "height": 0.7,
+                "weight": 6.9,
+            },
+            "stats": {
+                "total": 318,
+                "hp": 45,
+                "attack": 49,
+                "defense": 49,
+                "sp_attack": 65,
+                "sp_defense": 65,
+                "speed": 45,
+            },
+            "catch_rate": 45,
+            "base_friendship": 50,
+            "base_exp": 64,
+            "growth_rate": "MEDIUM_SLOW",
+            "male_percent": 87.5,
+            "gender_diffs": False,
+        }
+    actual: dict = example_pokemon.to_dict()
+
+    assert actual == expected
