8d169a576ba70d0577f2fd3bce5028c5ff133342
Daniel Moffett <moffd234@gmail.com>
2025-07-27 04:52:08 -0400

    Added offensive neutral interactions for Fire and Water Types

--- Application/Models/Types.py
+++ Application/Models/Types.py
@@ -30,39 +30,58 @@ class Types(Enum):
                "Psychic", "Ice", "Dragon", "Dark", "Fairy"],
               ["Normal", "Flying", "Poison", "Ground", "Rock", "Bug", "Steel", "Fire", "Water", "Grass", "Electric",
                "Psychic", "Ice", "Dragon", "Dark", "Fairy"])
+
     FIRE = (["Grass", "Bug", "Ice", "Steel"], ["Water", "Ground", "Rock"],
             ["Fire", "Grass", "Ice", "Steel", "Fairy", "Bug"],
-            ["Fire", "Water", "Rock", "Dragon"], [], [])
+            ["Fire", "Water", "Rock", "Dragon"], [], [],
+            ["Normal", "Fighting", "Flying", "Poison", "Ground", "Ghost", "Electric", "Psychic", "Dark", "Fairy"],
+            [None])
+
     WATER = (['Fire', "Ground", "Rock"], ["Grass", "Electric"], ["Steel", "Water", "Fire", "Ice"],
-             ["Water", "Grass", "Dragon"], [], [])
+             ["Water", "Grass", "Dragon"], [], [],
+             ["Normal", "Fighting", "Flying", "Poison", "Bug", "Ghost", "Steel", "Electric", "Psychic", "Ice", "Dark",
+              "Fairy"], [None])
     GRASS = (["Water", "Ground", "Rock"], ["Fire", "Flying", "Bug", "Poison", "Ice"],
              ["Fire", "Grass", "Poison", "Flying", "Bug", "Dragon", "Steel"], ["Water", "Electric", "Grass"], [], [])
 
     ELECTRIC = (["Water", "Flying"], ["Ground"], ["Electric", "Flying", "Steel"], ["Grass", "Electric", "Dragon"],
                 ["Ground"], [])
+
     ICE = (["Grass", "Ground", "Flying", "Dragon"], ["Fire", "Fighting", "Rock", "Steel"], ["Ice"],
            ["Fire", "Water", "Steel"], [], [])
+
     FIGHTING = (["Normal", "Steel", "Ice", "Rock", "Dark"], ["Psychic", "Fairy", "Flying"], ["Bug", "Rock", "Dark"],
                 ["Poison", "Flying", "Psychic", "Bug", "Fairy"], [], [])
+
     POISON = (["Grass", "Fairy"], ["Ground", "Psychic"], ["Grass", "Fighting", "Poison", "Bug", "Fairy"],
               ["Poison", "Ground", "Rock", "Ghost"], ["Steel"], [])
+
     GROUND = (["Electric", "Fire", "Poison", "Rock", "Steel"], ["Water", "Grass", "Ice"], ["Poison", "Rock"],
               ["Grass", "Bug"], ["Flying"], ["Electric"])
+
     FLYING = (["Fighting", "Grass", "Bug"], ["Electric", "Ice", "Rock"], ["Fighting", "Grass", "Bug"],
               ["Electric", "Rock", "Steel"], ["Ground"], [])
+
     PSYCHIC = (["Fighting", "Poison"], ["Bug", "Ghost", "Dark"], ["Fighting", "Psychic"], ["Psychic", "Steel"],
                ["Dark"], [])
+
     BUG = (["Grass", "Psychic", "Dark"], ["Fire", "Flying", "Rock"], ["Grass", "Fighting", "Ground"],
            ["Fire", "Fighting", "Poison", "Flying", "Ghost", "Steel", "Fairy"], [], [])
+
     ROCK = (["Fire", "Ice", "Flying", "Bug"], ["Water", "Grass", "Fighting", "Ground", "Steel"],
             ["Normal", "Fire", "Poison", "Flying"], ["Fighting", "Ground", "Steel"], [], [])
+
     GHOST = (["Psychic", "Ghost"], ["Ghost", "Dark"], ["Poison", "Bug"], ["Dark"], [], ["Normal", "Fighting"])
+
     DRAGON = (["Dragon"], ["Dragon", "Fairy"], ["Fire", "Grass", "Water", "Electric"], ["Steel"], [], ["Fairy"])
+
     DARK = (["Psychic", "Ghost"], ["Fighting", "Bug", "Fairy"], ["Ghost", "Dark"], ["Fighting", "Fairy"],
             [], ["Psychic"])
+
     STEEL = (["Ice", "Rock", "Fairy"], ["Fire", "Fighting", "Ground"],
              ["Normal", "Grass", "Ice", "Flying", "Psychic", "Bug", "Rock", "Dragon", "Steel", "Fairy"],
              ["Fire", "Water", "Electric", "Steel"], [], [])
+
     FAIRY = (["Dragon", "Fighting", "Dark"], ["Poison", "Steel"], ["Fighting", "Bug", "Dark"],
              ["Fire", "Poison", "Steel"], [], [])
 
