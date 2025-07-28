60a15004990408149470a0723b5c9c41a7e71bb9
Daniel Moffett <moffd234@gmail.com>
2025-07-28 17:35:21 -0400

    Added extra get_offensive_weaknesses tests for gen6

--- Test/test_TypeUtils.py
+++ Test/test_TypeUtils.py
@@ -5,7 +5,29 @@ def test_get_offensive_weakness_fire(type_utils):
     expected: list[str] = ["fire", "water", "rock", "dragon"]
     actual: list[str] = type_utils.get_offensive_weaknesses("fire")
 
-    assert actual == expected
+    assert sorted(actual) == sorted(expected)
+
+
+def test_get_offensive_weakness_grass(type_utils):
+    expected: list[str] = ["flying", "poison", "bug", "fire", "steel", "grass", "dragon"]
+    actual: list[str] = type_utils.get_offensive_weaknesses("grass")
+
+    assert sorted(actual) == sorted(expected)
+
+
+def test_get_offensive_weakness_ghost(type_utils):
+    expected: list[str] = ["dark"]
+    actual: list[str] = type_utils.get_offensive_weaknesses("ghost")
+
+    assert sorted(actual) == sorted(expected)
+
+
+def test_get_fairy_offensive_weakness(type_utils):
+    expected: list[str] = ["poison", "steel", "fire"]
+    actual: list[str] = type_utils.get_offensive_weaknesses("fairy")
+
+    assert sorted(actual) == sorted(expected)
+
 
 def test_get_offensive_weakness_fire_uppercase(type_utils):
     expected: list[str] = ["fire", "water", "rock", "dragon"]
@@ -13,11 +35,13 @@ def test_get_offensive_weakness_fire_uppercase(type_utils):
 
     assert actual == expected
 
+
 def test_get_offensive_weakness_fire_mix_case(type_utils):
     expected: list[str] = ["fire", "water", "rock", "dragon"]
     actual: list[str] = type_utils.get_offensive_weaknesses("fIrE")
 
     assert actual == expected
+
 
 def test_get_offensive_weakness_type_doesnt_exist(type_utils):
     with pytest.raises(ValueError) as error:
