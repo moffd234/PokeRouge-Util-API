9e32c7d7f2c33dbce8cc6357db6030c273dc58f3
Daniel Moffett <moffd234@gmail.com>
2025-07-29 17:57:20 -0400

    Wrote get_offensive_weaknesses for weaknesses_bp

--- Application/Routes/Weakness.py
+++ Application/Routes/Weakness.py
@@ -19,3 +19,12 @@ def get_weakness_summary(pokemon_species: str):
         return jsonify(output), 400
 
     return jsonify(output)
+
+@weaknesses_bp.route('weaknesses/offensive/<string:pokemon_type>', methods=['GET'])
+def get_offensive_weaknesses(pokemon_type: str):
+    output: list = type_util.get_offensive_weaknesses(pokemon_type)
+
+    if "error" in output:
+        return jsonify(output), 400
+
+    return jsonify(output)
