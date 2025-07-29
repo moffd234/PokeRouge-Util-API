b099ab06e55e4efef7b707a9752fc0c9ee9dbdf1
Daniel Moffett <moffd234@gmail.com>
2025-07-29 15:37:43 -0400

    Updated route for get_weakness_summary

--- Application/Routes/Weakness.py
+++ Application/Routes/Weakness.py
@@ -6,7 +6,7 @@ from Application.Utils.TypeUtils import TypeUtils
 weaknesses_bp: Blueprint = Blueprint("weaknesses_bp", __name__)
 type_util: TypeUtils = TypeUtils()
 
-@weaknesses_bp.route('/weaknesses/<string:pokemon_species>', methods=['GET'])
+@weaknesses_bp.route('/weaknesses/summary/<string:pokemon_species>', methods=['GET'])
 def get_weakness_summary(pokemon_species: str):
     pokemon: Pokemon | None = Pokemon.query.filter_by(name=pokemon_species.upper()).first()
 
