88fa855ff8c6463dc31b171122662eea08d23584
Daniel Moffett <moffd234@gmail.com>
2025-07-30 15:01:43 -0400

    Wrote call_and_catch

--- Application/Routes/Weakness.py
+++ Application/Routes/Weakness.py
@@ -13,6 +13,14 @@ def return_output(output: dict | list) -> tuple[Response, int] | Response:
 
     return jsonify(output)
 
+def call_and_catch(func: callable, *args, **kwargs) -> tuple[Response, int] | Response:
+    try:
+        output = func(*args, **kwargs)
+        return return_output(output)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 404
+
+
 
 @weaknesses_bp.route('/weaknesses/summary/<string:pokemon_species>', methods=['GET'])
 def get_weakness_summary(pokemon_species: str) -> tuple[Response, int] | Response:
@@ -27,9 +35,12 @@ def get_weakness_summary(pokemon_species: str) -> tuple[Response, int] | Respons
 
 @weaknesses_bp.route('/weaknesses/offensive/<string:pokemon_type>', methods=['GET'])
 def get_offensive_weaknesses(pokemon_type: str) -> tuple[Response, int] | Response:
-    output: list = type_util.get_offensive_weaknesses(pokemon_type)
+    try:
+        output: list = type_util.get_offensive_weaknesses(pokemon_type)
+        return return_output(output)
 
-    return return_output(output)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
 
 
 @weaknesses_bp.route('/weaknesses/defensive/<string:type_1>', methods=['GET'])
@@ -39,12 +50,14 @@ def get_defensive_weaknesses(type_1: str, type_2: str = None) -> tuple[Response,
 
     return return_output(output)
 
+
 @weaknesses_bp.route('/weaknesses/immunities/<string:type_1>', methods=['GET'])
 @weaknesses_bp.route('/weaknesses/immunities/<string:type_1>/<string:type_2>', methods=['GET'])
 def get_immunities(type_1: str, type_2: str = None) -> tuple[Response, int] | Response:
     output: list = type_util.get_immunities(type_1, type_2)
 
     return return_output(output)
+
 
 @weaknesses_bp.route('/weaknesses/immune-defenders/<string:type_1>', methods=['GET'])
 def get_immune_defenders(type_1: str) -> tuple[Response, int] | Response:
