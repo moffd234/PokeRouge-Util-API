2aca13863cb3bf8e089a742c1a847aea0334b767
Daniel Moffett <moffd234@gmail.com>
2025-07-31 23:04:31 -0400

    Added comments to separate what function is being tests where

--- Test/test_TypeUtils.py
+++ Test/test_TypeUtils.py
@@ -3,6 +3,9 @@ import pytest
 from Application.Models.Pokemon import Pokemon
 
 
+# ========================================================================================
+# get_offensive_weaknesses(self, atk_type: str) -> list[str]:
+# ========================================================================================
 @pytest.mark.parametrize("input_type, expected", [
     ("fire", ["fire", "water", "rock", "dragon"]), ("fIrE", ["fire", "water", "rock", "dragon"]),
     ("grass", ["flying", "poison", "bug", "fire", "steel", "grass", "dragon"]),
@@ -21,6 +24,10 @@ def test_get_offensive_weakness_type_doesnt_exist(type_utils, input_type):
 
     assert str(error.value) == f"'{input_type.title()}' is not a valid type"
 
+
+# ========================================================================================
+# get_defensive_weaknesses(self, type_1: str, type_2: str | None) -> list[str]:
+# ========================================================================================
 
 @pytest.mark.parametrize("input_type, expected", [
     ("fire", ["ground", "rock", "water"]), ("fIrE", ["ground", "rock", "water"]), ("normal", ["fighting"]),
@@ -60,6 +67,10 @@ def test_get_defensive_weakness_type_2_doesnt_exist(type_utils, input_type):
     assert str(error.value) == f"'{input_type.title()}' is not a valid type"
 
 
+# ========================================================================================
+# get_immunities(self, type_1: str, type_2) -> list[str]:
+# ========================================================================================
+
 @pytest.mark.parametrize("input_type, expected", [
     ("ghost", ["normal", "fighting"]), ("flying", ["ground"]), ("ground", ["electric"]), ("steel", ["poison"]),
     ("dark", ["psychic"]), ("normal", ["ghost"]), ("fairy", ["dragon"]), ("fAiRy", ["dragon"]), ("fire", [])
@@ -72,7 +83,8 @@ def test_get_immunities_valid_mono_type(type_utils, input_type, expected):
 
 @pytest.mark.parametrize("input_type_1, input_type_2, expected", [
     ("fire", "steel", ["poison"]), ("FiRe", "steel", ["poison"]), ("FiRe", "sTeEl", ["poison"]),
-    ("fire", "sTeEl", ["poison"]), ("ghost", "normal", ["fighting", "ghost", "normal"]), ("electric", "flying", ["ground"]),
+    ("fire", "sTeEl", ["poison"]), ("ghost", "normal", ["fighting", "ghost", "normal"]),
+    ("electric", "flying", ["ground"]),
     ("fire", "water", [])
 ])
 def test_get_immunities_valid_dual_type(type_utils, input_type_1, input_type_2, expected):
@@ -87,6 +99,7 @@ def test_get_immunities_type_does_not_exist_mono_type(type_utils, input_type):
 
     assert str(error.value) == f"'{input_type.title()}' is not a valid type"
 
+
 @pytest.mark.parametrize("input_type", ["sound", "", " ", " fire ", " Fire ", " f"])
 def test_get_immunities_does_not_exist_dual_type(type_utils, input_type):
     with pytest.raises(ValueError) as error:
@@ -94,17 +107,24 @@ def test_get_immunities_does_not_exist_dual_type(type_utils, input_type):
 
     assert str(error.value) == f"'{input_type.title()}' is not a valid type"
 
+
 def test_get_immunities_first_type_does_not_exist(type_utils):
     with pytest.raises(ValueError) as error:
         type_utils.get_immunities("sound", "fire")
 
     assert str(error.value) == f"'Sound' is not a valid type"
 
+
 def test_get_immunities_both_types_do_not_exist(type_utils):
     with pytest.raises(ValueError) as error:
         type_utils.get_immunities("sound", "music")
 
     assert str(error.value) == f"'Sound' is not a valid type"
+
+
+# ========================================================================================
+# get_immune_defenders(self, attacker: str) -> list[str]:
+# ========================================================================================
 
 def test_get_immune_defenders_normal(type_utils):
     expected: list[str] = ["ghost"]
@@ -175,6 +195,10 @@ def test_get_immune_defenders_mixed_case(type_utils):
     actual: list[str] = type_utils.get_immune_defenders("NoRmAl")
     assert actual == sorted(expected)
 
+
+# ========================================================================================
+# get_weakness_summary(self, pokemon_species: Pokemon):
+# ========================================================================================
 
 def test_get_weakness_summary_bulbasaur(type_utils, example_pokemon):
     expected: dict = {
