8d7ba5cebff0dcacf2b6a2b0b6ff7c1558569605
Daniel Moffett <moffd234@gmail.com>
2025-07-31 21:44:38 -0400

    Wrote parameterized tests for get_defensive_weakness_valid_types for dual_type

--- Test/test_TypeUtils.py
+++ Test/test_TypeUtils.py
@@ -33,37 +33,13 @@ def test_get_defensive_weakness_valid_type(type_utils, input_type, expected):
     assert actual == sorted(expected)
 
 
-def test_get_defensive_weakness_fire_grass(type_utils):
-    expected: list[str] = ["flying", "poison", "rock"]
-    actual: list[str] = type_utils.get_defensive_weaknesses("fire", "grass")
-
-    assert actual == sorted(expected)
-
-
-def test_get_defensive_weakness_ghost_normal(type_utils):
-    expected: list[str] = ["dark"]
-    actual: list[str] = type_utils.get_defensive_weaknesses("ghost", "normal")
-
-    assert actual == sorted(expected)
-
-
-def test_get_defensive_weakness_mixed_case_type_1(type_utils):
-    expected: list[str] = ["flying", "poison", "rock"]
-    actual: list[str] = type_utils.get_defensive_weaknesses("FIRe", "grass")
-
-    assert actual == sorted(expected)
-
-
-def test_get_defensive_weakness_mixed_case_type_2(type_utils):
-    expected: list[str] = ["flying", "poison", "rock"]
-    actual: list[str] = type_utils.get_defensive_weaknesses("fire", "gRasS")
-
-    assert actual == sorted(expected)
-
-
-def test_get_defensive_weakness_mixed_case_both_type(type_utils):
-    expected: list[str] = ["flying", "poison", "rock"]
-    actual: list[str] = type_utils.get_defensive_weaknesses("FiRe", "gRasS")
+@pytest.mark.parametrize("input_type_1, input_type_2, expected", [
+    ("fire", "grass", ["flying", "poison", "rock"]), ("fIrE", "grass", ["flying", "poison", "rock"]),
+    ("fire", "GrAsS", ["flying", "poison", "rock"]), ("fIrE", "GrAsS", ["flying", "poison", "rock"]),
+    ("ghost", "normal", ["dark"]), ("normal", "ghost", ["dark"]), ("normal", "normal", ["fighting"])
+])
+def test_get_defensive_weakness_valid_dual_type(type_utils, input_type_1, input_type_2, expected):
+    actual: list[str] = type_utils.get_defensive_weaknesses(input_type_1, input_type_2)
 
     assert actual == sorted(expected)
 
