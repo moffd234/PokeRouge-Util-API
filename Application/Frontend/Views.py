fe0d091d6351ab41e19698bd9c8eb73251b5c952
Daniel Moffett <moffd234@gmail.com>
2025-08-02 02:23:59 -0400

    Added footer to base.html

--- Application/Frontend/Views.py
+++ Application/Frontend/Views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template
 
 views_bp: Blueprint = Blueprint('views_bp', __name__)
@@ -5,3 +7,7 @@ views_bp: Blueprint = Blueprint('views_bp', __name__)
 @views_bp.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
+
+@views_bp.app_context_processor
+def inject_current_year():
+    return {'year': datetime.now().year}
