22576523005f9b451fc80b832e8992b5e3e881dc
Daniel Moffett <moffd234@gmail.com>
2025-08-04 15:32:34 -0400

    Created weaknesses page

--- Application/Frontend/Views.py
+++ Application/Frontend/Views.py
@@ -4,10 +4,16 @@ from flask import Blueprint, render_template
 
 views_bp: Blueprint = Blueprint('views_bp', __name__)
 
-@views_bp.route("/", methods=['GET'])
-def index():
-    return render_template("index.html")
 
 @views_bp.app_context_processor
 def inject_current_year():
     return {'year': datetime.now().year}
+
+
+@views_bp.route("/", methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@views_bp.route("/weaknesses", methods=['GET'])
+def weaknesses():
+    return render_template("weaknesses.html")
