18c182f1b1e478883cc01ed75091ed515c2a5a22
Daniel Moffett <moffd234@gmail.com>
2025-08-11 15:00:52 -0400

    Added example_team fixture and type_dict_zeros

--- Test/test_TeamAnalyzer.py
+++ Test/test_TeamAnalyzer.py
@@ -1,20 +1,17 @@
 from Application.Models.Pokemon import Pokemon
-from Application.Utils.TeamAnalyzer import get_team_resistances
+from Application.Utils.TeamAnalyzer import get_team_resistances, get_team_immunities
+from Test.conftest import example_team
 
+type_dict_zeros: dict[str, int] = {"normal": 0, "fire": 0, "water": 0, "electric": 0, "grass": 0, "ice": 0,
+                                   "fighting": 0, "poison": 0, "ground": 0, "flying": 0, "psychic": 0, "bug": 0,
+                                   "rock": 0, "ghost": 0, "dragon": 0, "dark": 0, "steel": 0, "fairy": 0}
 
-def test_get_team_resistances_full_team():
-    team: list[Pokemon] = [Pokemon.query.filter_by(name="BULBASAUR").first(),
-                           Pokemon.query.filter_by(name="PIKACHU").first(),
-                           Pokemon.query.filter_by(name="INFERNAPE").first(),
-                           Pokemon.query.filter_by(name="EMPOLEON").first(),
-                           Pokemon.query.filter_by(name="GOLEM").first(),
-                           Pokemon.query.filter_by(name="ABRA").first()]
-
+def test_get_team_resistances_full_team(example_team):
     expected: dict = {"normal": 2, "fire": 2, "water": 2, "electric": 2, "grass": 2, "ice": 2, "fighting": 2,
                       "poison": 1, "ground": 0, "flying": 3, "psychic": 2, "bug": 2, "rock": 2, "ghost": 0,
                       "dragon": 1, "dark": 1, "steel": 3, "fairy": 2}
 
-    actual: dict = get_team_resistances(team)
+    actual: dict = get_team_resistances(example_team)
 
     assert actual == expected
 
@@ -47,14 +44,13 @@ def test_get_team_resistances_all_bulbasaur():
 
     assert actual == expected
 
+
 def test_get_team_resistances_none():
     team: list[Pokemon] = []
-    expected: dict = {"normal": 0, "fire": 0, "water": 0, "electric": 0, "grass": 0, "ice": 0, "fighting": 0,
-                      "poison": 0, "ground": 0, "flying": 0, "psychic": 0, "bug": 0, "rock": 0, "ghost": 0,
-                      "dragon": 0, "dark": 0, "steel": 0, "fairy": 0}
     actual: dict = get_team_resistances(team)
 
-    assert actual == expected
+    assert actual == type_dict_zeros
+
 
 def test_get_team_resistances_all_types_resisted():
     team: list[Pokemon] = [Pokemon.query.filter_by(name="GYARADOS").first(),
@@ -62,6 +58,7 @@ def test_get_team_resistances_all_types_resisted():
                            Pokemon.query.filter_by(name="HOUNDOOM").first(),
                            Pokemon.query.filter_by(name="GOLEM").first(),
                            Pokemon.query.filter_by(name="TANGELA").first()]
+
     expected: dict = {"normal": 2, "fire": 3, "water": 2, "electric": 1, "grass": 2, "ice": 1, "fighting": 1,
                       "poison": 1, "ground": 1, "flying": 2, "psychic": 1, "bug": 2, "rock": 2, "ghost": 1,
                       "dragon": 1, "dark": 1, "steel": 3, "fairy": 1}
@@ -69,14 +66,12 @@ def test_get_team_resistances_all_types_resisted():
 
     assert actual == expected
 
+
 def test_get_team_resistances_no_resistances():
     team: list[Pokemon] = [Pokemon.query.filter_by(name="EEVEE").first(),
                            Pokemon.query.filter_by(name="DITTO").first(),
                            Pokemon.query.filter_by(name="SNORLAX").first()]
 
-    expected: dict = {"normal": 0, "fire": 0, "water": 0, "electric": 0, "grass": 0, "ice": 0, "fighting": 0,
-                      "poison": 0, "ground": 0, "flying": 0, "psychic": 0, "bug": 0, "rock": 0, "ghost": 0,
-                      "dragon": 0, "dark": 0, "steel": 0, "fairy": 0}
     actual: dict = get_team_resistances(team)
 
-    assert actual == expected
+    assert actual == type_dict_zeros
